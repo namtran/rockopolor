7367ea933817f86f0b1a9846d7b2ff53a1c89293
Tran Nam <vannamtran@gmail.com>
2025-06-15 19:50:08 +0700

    no message

--- src/App.jsx
+++ src/App.jsx
@@ -29,7 +29,7 @@ function App() {
 
     return (
       <div className="w-full flex justify-center px-4 sm:px-8 md:px-0">
-        <nav className="flex items-center flex-wrap justify-center gap-x-[10px] md:gap-x-[50px] menu-right">
+        <nav className="nav-menu flex items-center flex-wrap justify-center gap-x-[10px] md:gap-x-[50px] menu-right">
 
           {sections.map((section) => (
             <Link
